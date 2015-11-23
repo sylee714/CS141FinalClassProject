426fd430ca5a4593fd31d7e4754acb98289fc8f5
jeffrey475 <jeffrey475@yahoo.com>
2015-11-23 11:00:39 -0800

    Update GameSaveData.java

--- src/edu/cpp/cs/cs141/prog_assgmnt_final/GameSaveData.java
+++ src/edu/cpp/cs/cs141/prog_assgmnt_final/GameSaveData.java
@@ -9,6 +9,24 @@ package edu.cpp.cs.cs141.prog_assgmnt_final;
  */
 public class GameSaveData {
   //need the positions of the player, enemies, powerups, and briefcase.
+  playerinfo()
+  {
+    getLife()
+    {
+     int Life = player.setLife();
+     return Life;
+    }
+    getRadar()
+    {
+      boolean radar = player.setRadar();
+      return radar;
+    }
+    getInvincibility()
+    {
+      boolean invincibility = player.setInvincibile();
+      return invincibility;
+    }
+  }
   
   //need the lives, bullets, powerups currently using, powerups left, enemies left
 
