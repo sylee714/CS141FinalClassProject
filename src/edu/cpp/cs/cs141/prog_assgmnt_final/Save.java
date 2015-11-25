dd112d3bbc86dfc4b55ab6887f8207acea6e32e6
jeffrey475 <jeffrey475@yahoo.com>
2015-11-24 19:50:22 -0800

    Create Save.java

---
+++ src/edu/cpp/cs/cs141/prog_assgmnt_final/Save.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.Scanner;
+/*
+This is the save method.
+*/
+
+public class Save()
+{
+
+/*
+  General Output Stream Format:
+  
+  FileOutputStream outStream = new FileOutputStream("Objects.dat");
+  ObjectOutputStream objectOutputFile = new ObjectOutputStream(outStream);
+  objectOutputFile.writeObject();
+  objectOutputFile.close();
+*/
+
+
+}
